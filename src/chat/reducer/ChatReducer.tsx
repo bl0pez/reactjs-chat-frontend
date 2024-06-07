ec51bb27d87c129e095c54ec5e43c0f109dc3366
bryan lopez <blopez4434@gmail.com>
2024-06-07 09:34:05 -0400

    refactor and update ChatReducer to handle new messages

--- src/chat/reducer/ChatReducer.tsx
+++ src/chat/reducer/ChatReducer.tsx
@@ -1,9 +1,9 @@
-import { ChatState } from "@/context/ChatContext";
-import { User } from "@/interfaces";
+import { ChatState, Message, User } from "@/interfaces";
 
-export type ChatAction = 
-    { type: "SaveUsers"; payload: User[] }
-  | { type: "SelectChat"; payload: { id: string } };
+export type ChatAction =
+  | { type: "SaveUsers"; payload: User[] }
+  | { type: "SelectChat"; payload: { id: string } }
+  | { type: "NewMessage"; payload: Message };
 export const chatReducer = (
   state: ChatState,
   action: ChatAction
@@ -15,14 +15,26 @@ export const chatReducer = (
         users: action.payload,
       };
     case "SelectChat":
-
       if (state.chatActive === action.payload.id) return state;
 
       return {
         ...state,
         chatActive: action.payload.id,
         messages: [],
+      };
+    case "NewMessage":
+      if (
+        state.chatActive === action.payload.from ||
+        state.chatActive === action.payload.to
+      ) {
+        return {
+          ...state,
+          messages: [...state.messages, action.payload],
+        };
+      } else {
+        return state;
       }
+
     default:
       return state;
   }
